cafcac09f9cb562648c7d01d4b29738c859a29bd
zeropointsix <kirill-logvinov@bk.ru>
2020-12-04 09:18:07 +0300

    switch to unittest

--- test_sample.py
+++ test_sample.py
@@ -1,5 +1,8 @@
+import unittest
+
 def inc(x):
     return x + 1
 
-def test_answer():
-    assert inc(3) == 4
+class cool_test(unittest.TestCase):
+    def test_it(self):
+    	self.assertEqual(inc(3), 4)
